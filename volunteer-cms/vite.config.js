7efe50e5f94c5f97e6ced5e78d1357aacd266d4e
Emarwalker <63945241+Emarwalker@users.noreply.github.com>
2025-02-21 00:55:58 +0700

    Update vite.config.js

--- volunteer-cms/vite.config.js
+++ volunteer-cms/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // เปิดให้เข้าถึงจากเครือข่ายภายนอก (จำเป็นสำหรับ Render)
+    port: process.env.PORT || 4173, // ใช้พอร์ตจาก Render หรือ fallback เป็น 4173
+  }
 })
