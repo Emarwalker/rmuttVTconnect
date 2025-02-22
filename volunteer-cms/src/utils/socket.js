b8d89ddcd87b52f8176bbb77622119abc9bbc723
Emarwalker <63945241+Emarwalker@users.noreply.github.com>
2025-02-23 03:06:09 +0700

    Update socket.js

--- volunteer-cms/src/utils/socket.js
+++ volunteer-cms/src/utils/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-const socket = io(import.meta.env.VITE_API_URL || 'http://localhost:5001 || 'https://emarwalker.taileb79c1.ts.net');
+const socket = io(import.meta.env.VITE_API_URL || 'https://emarwalker.taileb79c1.ts.net' || 'http://localhost:5001');
 
 export default socket;
