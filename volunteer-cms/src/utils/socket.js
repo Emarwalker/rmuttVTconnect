80dfbed28d3565650fec01970f12d5f4e97c2752
Emarwalker <63945241+Emarwalker@users.noreply.github.com>
2025-02-25 00:37:08 +0700

    Update socket.js

--- volunteer-cms/src/utils/socket.js
+++ volunteer-cms/src/utils/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-const socket = io(import.meta.env.VITE_API_URL || 'https://emarwalker.taileb79c1.ts.net' || 'http://localhost:5001');
+const socket = io(import.meta.env.VITE_API_URL || 'https://emarwalker.taileb79c1.ts.net' || 'https://project-back-deploy100.onrender.com/');
 
 export default socket;
