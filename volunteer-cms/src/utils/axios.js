d1d9eeb5feec9329332eb5b7e32a0b2b26822646
Emarwalker <63945241+Emarwalker@users.noreply.github.com>
2025-03-09 01:41:48 +0700

    Update axios.js

--- volunteer-cms/src/utils/axios.js
+++ volunteer-cms/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://project-back-deploy100.onrender.com/api',
+  baseURL: 'https://project-back-deploy100-g88h.onrender.com/api',
   timeout: 60000,  
   withCredentials: true, // ✅ ถูกต้อง
   headers: {
